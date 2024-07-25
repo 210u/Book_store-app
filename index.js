9db264d16a4523be2aff390857b75e0074f979ac
vanina raidel <146800796+210u@users.noreply.github.com>
2024-07-25 13:40:32 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||5000;
+const cors =require('cors')
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//Coolgirl2468*
+
+app.get('/', (req, res) => { 
+  res.send('Hello World!')
+})
+
+
+
+//mongodb configuration
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:Coolgirl2468*@cluster0.rex8x59.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    //create a collection of documents
+    const bookcollections = client.db("bookinventory").collection("books");
+
+    //insert a book to he db: post method
+
+    app.post("/upload.book", async(req, res) =>{
+      const data = rep.body;
+      const result = await bookcollections.insertOne(data);
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
